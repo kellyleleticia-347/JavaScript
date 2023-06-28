a5dbc2b44399befbfe056ba49336dbf46711c730
kelly <kellyleleticia347@gmail.com>
2023-06-27 21:55:53 -0300

    ex em js

---
+++ aula011/ex008.js
@@ -0,0 +1,7 @@
+var vel = 58.2
+console.log(`a velocidade do seu carro é ${vel} km/h !`)
+if (vel > 60 ) {
+    console.log(`você ultrpassou a velocidade . MULTADO !`)
+}
+
+console.log(`Dirija sempre com sinto de segurança !`)
---
+++ aula011/ex009.js
@@ -0,0 +1,8 @@
+var país = ' brasil'
+console.log(`você está vivendo em ${país}`)
+if (país == ' brasil') {
+    console.log(`você é Brasileiro`)
+}
+else {
+    console.log(`você é estrangeiro`)
+}
