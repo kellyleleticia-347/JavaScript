118eb10fc6e3e0a8508b54ba142a322a86cdfc0a
kelly <kellyleleticia347@gmail.com>
2023-07-25 20:41:35 -0300

    atualizações

---
+++ aula013/ambiente.js
@@ -0,0 +1,16 @@
+var c = 1
+do  { 
+    console.log('Tudo bem ?')
+    c++
+}while (c <= 5)
+
+
+ console.log('terminou !')
+
+
+ /*var c = 1
+while (c <= 5) { 
+    console.log('Tudo bem ?')
+    c++
+}
+ console.log('terminou !')*/
---
+++ aula014/ambiente.js
@@ -0,0 +1,6 @@
+
+console.log('inicío')
+for (let c = 1; c <= 20;c++) {
+    console.log(c)
+}
+console.log('fim')
